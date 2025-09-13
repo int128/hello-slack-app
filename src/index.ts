cc606e4d995610ff9eebe784859e70249b1785b9
Hidetake Iwata <int128@gmail.com>
2025-09-13 21:30:44 +0900

    Handle app mention

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, Slack App!");
+import bolt from '@slack/bolt'
+
+const main = async () => {
+  const app = new bolt.App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+  })
+
+  app.event('app_mention', async ({ event, say }) => {
+    app.logger.info(event)
+    await say(`Hi there, <@${event.user}>!`)
+  })
+
+  await app.start()
+  app.logger.info('⚡️ Bolt app is running!')
+}
+
+await main()
